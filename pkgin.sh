06430b8b6b9388479c6df331b2cd701d245457a9
Rainbow <samantha@smsittech.com>
2016-01-17 13:11:52 -0500

    Create pkgin.sh

---
+++ pkgin.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "This is the Pür Linux pkgin bootstrapper"
+echo "It works similarly to the bootstraper for pkgng on FreeBSD"
+echo "This file is located in /usr/bin"
+echo "Once it has installed pkgin in /usr/local/bin, it will autoremove itself"
+if ls /usr/bin | grep -q ports ;then
+	echo "Bootstrapping the Ports Tree"
+	/usr/bin/ports
+fi
+cd /usr/ports/databases/sqlite3 && make install clean
+cd /usr/ports/pkgtools/libnbcompat && make install clean
+cd /usr/ports/net/libfetch && make install clean
+cd /usr/ports/archivers/libarchive && make install clean
+cd /usr/ports/pkgtools/pkgin && make install clean
+rm /usr/bin/pkgin
