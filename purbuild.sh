da85135aa675f7f8169f6ac81215e8882378cbdf
Rainbow <samantha@smsittech.com>
2016-01-21 03:18:44 -0500

    Update purbuild.sh

--- purbuild.sh
+++ purbuild.sh
@@ -279,10 +279,10 @@ make
 make install
 
 # binutils pass 2
-mkdir -v $PSRC/binutils-build
-# cd $PSRC/binutils-2.25
-# make distclean ## Maybe I didn't read over binutils README well enough, but any reason we're adding this?
-cd $PSRC/binutils-build
+# mkdir -v $PSRC/binutils-build ## extraneous mkdir
+cd $PSRC/binutils-2.25
+make distclean ## Maybe I didn't read over binutils README well enough, but any reason we're adding this?
+# cd $PSRC/binutils-build
 CC=$PUR_TGT-gcc                \
 AR=$PUR_TGT-ar                 \
 RANLIB=$PUR_TGT-ranlib         \
